177dee5140ce19922a19ee5590f0c3347092c9c3
SanzidAhmed <sanzid.official@gmail.com>
2024-05-26 01:18:49 +0600

    after create a studentController and send student data in mongodb

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { studentService } from "./student.service";
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    // will call service function to send this data
+    const result = await studentService.createStudentIntoDB(studentData);
+    // send response
+    res.status(200).json({
+      success: true,
+      message: "Student created successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await studentService.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: "Student are retrieved successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+};
